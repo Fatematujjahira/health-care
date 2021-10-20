78d0db811750126890184becb6eae41f53c00053
fatematujjahira <zohrafatema357@gmail.com>
2021-10-20 23:00:13 +0600

    finis site

--- src/pages/service/Service.js
+++ src/pages/service/Service.js
@@ -18,10 +18,32 @@ const Service = ({ service }) =>
              <div className="text-area">
               <h4 className=" name fw-bold text-info p-2">{name}</h4>
                <h5 className="fw-bold ">{title}</h5>
-              <p>{discription}</p>
+              
+                 <p>{ discription }</p>
+                 <div className="">
+                <div className="d-flex justify-content-center ">
+               
+                <div className="m-2 ">
+                <h3><i className="fab fa-facebook-square icon text-primary"></i></h3>
+                </div>
+                <div className="m-2 ">
+                <h3><i className="fab fa-instagram icon text-danger"></i></h3>
+                </div>
+                <div className="m-2">
+                <h3><i className="fab fa-linkedin icon text-primary"></i></h3>
+                </div>
+                <div className="m-2">
+                <h3><i className="fab fa-youtube text-danger icon  "></i></h3>
+                </div>
+                <div className="m-2">
+                <h3><i className="fab fa-twitter icon text-primary"></i></h3>
+                </div>
+                </div>
+               
+               </div>
               <Link to={`/details/${id}`}>
               <button className="btn btn-outline-primary px-5 m-2">{name}</button>
-    
+      
                 </Link>
                 </div>
                
